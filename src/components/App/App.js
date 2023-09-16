9455b37bcafd9398706d9ed74a07cb22a8fdc33c
Grigor Ohanyan <ohanyang183@gmail.com>
2023-09-16 18:00:11 +0400

    my commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import './app.css'
+import Register from "../Register";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Register/>
+            </div>
+        )
+    }
+}
+
+export default App;
